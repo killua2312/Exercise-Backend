43f63f6b236ce5260793df65ea64b82e4e8dd382
killua2312 <rahulthota123@gmail.com>
2024-08-04 20:00:37 +0530

    cors changed

--- server.js
+++ server.js
@@ -7,20 +7,13 @@ const cors = require("cors");
 const app = express();
 const port = parseInt(process.env.EXPRESS_PORT);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
+const allowedOrigin = "https://aquamarine-stardust-a0b586.netlify.app/";
+const corsOptions = {
+  origin: allowedOrigin,
+  optionsSuccessStatus: 200,
+};
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 
