5d56d2bf8fe2b1bdb75d8aa542bd4a177d3200f4
killua2312 <rahulthota123@gmail.com>
2024-08-05 12:15:41 +0530

    ratelimit added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 const { sequelize } = require("./models");
 const exerciseRoutes = require("./routes/exerciseRoutes");
 const cors = require("cors");
@@ -7,7 +9,7 @@ const cors = require("cors");
 const app = express();
 const port = parseInt(process.env.EXPRESS_PORT);
 
-const allowedOrigin = "https://aquamarine-stardust-a0b586.netlify.app";
+const allowedOrigin = process.env.ALLOW_ORIGIN;
 const corsOptions = {
   origin: allowedOrigin,
   optionsSuccessStatus: 200,
@@ -17,7 +19,27 @@ app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 
-app.use("/api", exerciseRoutes);
+const createRateLimiter = (windowMs, max) => {
+  return rateLimit({
+    windowMs,
+    max,
+    message: { error: "Too many requests, please try again later." },
+    keyGenerator: (req) => req.apiKey,
+  });
+};
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 50,
+  delayMs: 1000,
+});
+
+app.use(
+  "/api",
+  createRateLimiter(15 * 60 * 1000, 50),
+  speedLimiter,
+  exerciseRoutes,
+);
 
 sequelize.sync().then(() => {
   app.listen(port, () => {
